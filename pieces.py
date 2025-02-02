90ad94583905bbb313c3cd43593697bcb022b9c5
Aengus Roberts <asr66@bath.ac.uk>
2025-02-02 20:50:53 +0000

    Added piece classes

--- pieces.py
+++ pieces.py
@@ -8,6 +8,25 @@ class Piece:
     def get_legal_moves(self, position, board):
         raise NotImplementedError("This method should be implemented by subclasses.")
 
+def slide_moves(position, board, directions, color):
+    moves = []
+    row, col = position
+
+    for d_row, d_col in directions:
+        r, c = row + d_row, col + d_col
+        while 0 <= r < 8 and 0 <= c < 8:
+            target = board[r][c]
+            if target == " ":
+                moves.append((r, c))
+            elif target[0] != color:
+                moves.append((r, c))
+                break  # Can capture but not move past
+            else:
+                break  # Blocked by own piece
+            r += d_row
+            c += d_col
+
+    return moves
 
 class Pawn(Piece):
     def get_legal_moves(self, position, board):
@@ -30,5 +49,58 @@ class Pawn(Piece):
                 target = board[row + direction][new_col]
                 if target != " " and target[0] != self.color:
                     moves.append((row + direction, new_col))
+
+        return moves
+
+class Rook(Piece):
+    def get_legal_moves(self, position, board):
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1)]  # Up, Down, Left, Right
+        return slide_moves(position, board, directions, self.color)
+
+class Bishop(Piece):
+    def get_legal_moves(self, position, board):
+        directions = [(-1, -1), (-1, 1), (1, -1), (1, 1)]  # Diagonals
+        return slide_moves(position, board, directions, self.color)
+
+class Queen(Piece):
+    def get_legal_moves(self, position, board):
+        directions = [(-1, 0), (1, 0), (0, -1), (0, 1),  # Rook moves
+                      (-1, -1), (-1, 1), (1, -1), (1, 1)]  # Bishop moves
+        return slide_moves(position, board, directions, self.color)
+
+class Knight(Piece):
+    def get_legal_moves(self, position, board):
+        moves = []
+        row, col = position
+        knight_moves = [
+            (2, 1), (1, 2), (-1, 2), (-2, 1),
+            (-2, -1), (-1, -2), (1, -2), (2, -1)
+        ]
+
+        for dr, dc in knight_moves:
+            r, c = row + dr, col + dc
+            if 0 <= r < 8 and 0 <= c < 8:
+                target = board[r][c]
+                if target == " " or target[0] != self.color:
+                    moves.append((r, c))
+
+        return moves
+
+class King(Piece):
+    def get_legal_moves(self, position, board):
+        moves = []
+        row, col = position
+        king_moves = [
+            (-1, -1), (-1, 0), (-1, 1),
+            (0, -1),          (0, 1),
+            (1, -1),  (1, 0), (1, 1)
+        ]
+
+        for dr, dc in king_moves:
+            r, c = row + dr, col + dc
+            if 0 <= r < 8 and 0 <= c < 8:
+                target = board[r][c]
+                if target == " " or target[0] != self.color:
+                    moves.append((r, c))
 
         return moves
